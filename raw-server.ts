542ad7f2278cd3aafaef7de26e66f3d1b36ea8e6
anees028 <rehmananees028@gmail.com>
2025-12-25 15:43:16 +0100

    Update callback : based on if-else, give response for the correct route.

--- raw-server.ts
+++ raw-server.ts
@@ -2,9 +2,27 @@ import * as http from 'http';
 
 const PORT = 3000;
 
+// In Node.js, the Internet works exactly the same way:
+// The Request (req) is a Readable Stream (Data coming from the user).
+// The Response (res) is a Writable Stream (Data you send back).
+
 const server = http.createServer((req, res) => {
-  // This function runs EVERY time a request hits the server.
-  console.log(`Incoming request: ${req.method} ${req.url}`);
+  // 1. Handle the URL and Method
+  if (req.url === '/status' && req.method === 'GET') {
+    // 2. Set Headers (Important for Clients/Browsers)
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    // 3. Write data to the stream
+    res.write(JSON.stringify({ message: 'Server is active', uptime: process.uptime() }));
+    
+    // 4. Close the stream
+    res.end();
+  } 
+  else {
+    // Handle 404
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Route not found' }));
+  }
 });
 
 server.listen(PORT, () => {
